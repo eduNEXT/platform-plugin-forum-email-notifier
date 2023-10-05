f094d46af813940c20fd9f6508770cf7f2c5b38c
Cristhian Garcia <crisgarta8@gmail.com>
2023-10-05 09:10:17 -0500

    fix: add missing template configuration

--- platform_plugin_forum_email_notifier/settings/common.py
+++ platform_plugin_forum_email_notifier/settings/common.py
@@ -5,6 +5,8 @@ https://docs.djangoproject.com/en/2.22/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.22/ref/settings/
 """
+import os
+from os.path import abspath, dirname, join
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.22/howto/deployment/checklist/
@@ -27,6 +29,34 @@ TIME_ZONE = "UTC"
 
 USE_TZ = True
 
+here = lambda *x: join(abspath(dirname(__file__)), *x)
+PROJECT_ROOT = here("..")
+root = lambda *x: join(abspath(PROJECT_ROOT), *x)
+
+# TEMPLATE CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.8/ref/settings/#templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'DIRS': (
+            root('templates'),
+        ),
+        'OPTIONS': {
+            'context_processors': (
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'credentials.apps.core.context_processors.core',
+            ),
+            'debug': True,  # Django will only display debug pages if the global DEBUG setting is set to True.
+        }
+    },
+]
 
 def plugin_settings(settings):
     """
