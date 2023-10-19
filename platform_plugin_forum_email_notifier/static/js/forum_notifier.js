840c767074fa35d6d213f20cda21eba278d426e2
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2023-10-19 11:51:29 -0500

    feat: add documentation in instructor dashboard (#7)

--- platform_plugin_forum_email_notifier/static/js/forum_notifier.js
+++ platform_plugin_forum_email_notifier/static/js/forum_notifier.js
@@ -1,33 +1,45 @@
 function getCookie(name) {
   let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-          }
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
       }
+    }
   }
   return cookieValue;
 }
 
-
-$('.email-notifications-select').on('change', function(select) {
-  fetch('instructor/api/forum_email_notification_preference', {
-    method: 'PUT',
+$(".email-notifications-select").on("change", function (select) {
+  fetch("instructor/api/forum_email_notification_preference", {
+    method: "PUT",
     body: JSON.stringify({
-      'preference': parseInt($(this).val())
+      preference: parseInt($(this).val()),
     }),
     headers: {
-      'Content-Type': 'application/json',
-      'X-CSRFToken': getCookie('csrftoken')
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCookie("csrftoken"),
     },
-    }).then(function(response) {
+  })
+    .then(function (response) {
       return response.json();
-    }).then(function(data) {
+    })
+    .then(function (data) {
       console.log(data);
     });
+});
+
+$(function () {
+  const cssUrl = "https://cdn.jsdelivr.net/npm/@edx/paragon@20.45.5/dist/paragon.min.css";
+
+  $("<link>")
+    .attr({
+      href: cssUrl,
+      rel: "stylesheet",
+    })
+    .appendTo("head");
 });
