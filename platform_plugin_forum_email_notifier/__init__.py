38dafdc950c78ed200f1eebf2a2e8067bc19b71b
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2023-10-23 14:50:18 -0500

    fix: add `locale` folder in `MANIFEST.in` (#9)
    * fix: add locale folder in MANIFEST.in
    
    * docs: update changelog for latest changes and bumping the version

--- platform_plugin_forum_email_notifier/__init__.py
+++ platform_plugin_forum_email_notifier/__init__.py
@@ -4,6 +4,6 @@ One-line description for README and other doc files.
 import os
 from pathlib import Path
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
