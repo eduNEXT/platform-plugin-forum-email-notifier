8bd5c2445d57e4489f79fc836c3eae33292994a8
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2023-10-24 12:02:12 -0500

    fix: use `recursive-include` to add `locale` folder (#10)
    * fix: use recursive include
    
    * docs: update changelog for latest changes and bumping the version

--- platform_plugin_forum_email_notifier/__init__.py
+++ platform_plugin_forum_email_notifier/__init__.py
@@ -4,6 +4,6 @@ One-line description for README and other doc files.
 import os
 from pathlib import Path
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
